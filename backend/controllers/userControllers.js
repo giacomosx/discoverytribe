e4350bd49187ced24a4458f1803b61267b926dc3
= <giacomo.bartoli@me.com>
2024-06-15 19:47:21 +0200

    follow, unfollow, delete, edit user add

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -8,7 +8,7 @@ const getCurrentUser = async (req, res, next) => {
 
         res.status(200).json(user)
     } catch (e) {
-        next(e);
+        next({statusCode: 500, message: e.message});
     }
 }
 
@@ -20,11 +20,95 @@ const getUserById = async (req, res, next) => {
 
         res.status(200).json(user)
     } catch (e) {
-        next(e);
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const followUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.userId)
+        const userToFollow = await User.findById(req.params.id)
+
+        user.followings.push(req.params.id);
+        userToFollow.followers.push(user._id);
+
+        user.save()
+        userToFollow.save()
+
+        res.status(201).json({message:"Successfully following user"})
+    } catch (e) {
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const unfollowUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.userId)
+        const userToUnfollow = await User.findById(req.params.id)
+
+        user.followings.pull(req.params.id);
+        userToUnfollow.followers.pull(user._id);
+
+        user.save()
+        userToUnfollow.save()
+
+        res.status(201).json({message: "Successfully unfollowing user"})
+    } catch (e) {
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const getFollowings = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.userId).populate('followings').select('followings')
+        res.status(200).json(user)
+
+    } catch (e) {
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const getFollowers = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user.userId).populate('followers').select('followers')
+        res.status(200).json(user)
+
+    } catch (e) {
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const user = await User.findByIdAndDelete(req.user.userId)
+
+        res.status(200).json({message:"Successfully deleted user", user})
+
+    } catch (e) {
+        next({statusCode: 500, message: e.message});
+    }
+}
+
+const editUser = async (req, res, next) => {
+    try {
+        const editedUser = await User.findByIdAndUpdate(req.user.userId, {
+            ...req.body
+        }, {new: true})
+
+        res.status(200).json({message:"Successfully edited user", editedUser})
+    } catch (e) {
+
+        res.status(400).json({message: e.message});
     }
 }
 
 module.exports = {
     getCurrentUser,
-    getUserById
+    getUserById,
+    followUser,
+    getFollowings,
+    getFollowers,
+    unfollowUser,
+    deleteUser,
+    editUser
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,27 +2,21 @@ const express = require('express');
 const user = express.Router();
 const controller = require('../controllers/userControllers');
 
-
 user.route('/me').get(controller.getCurrentUser)
 
+user.route('/me/edit').patch(controller.editUser)
+
+user.route('/me/delete').delete(controller.deleteUser)
+
+user.route('/me/followings').get(controller.getFollowings)
+
+user.route('/me/followers').get(controller.getFollowers)
+
+user.route('/:id/follow').patch(controller.followUser)
+
+user.route('/:id/unfollow').patch(controller.unfollowUser)
+
 user.route('/:id').get(controller.getUserById)
-
-
-user.route('/:id').put((req, res) => {
-
-})
-
-user.route('/:id').delete((req, res) => {
-
-})
-
-user.route('/:id/follow').put((req, res) => {
-
-})
-
-user.route('/:id/unfollow').put((req, res) => {
-
-})
 
 
 module.exports = user
